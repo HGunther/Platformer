3739298a7cee1d562e7178649c70db7ac4d4c619
hgunther <hnsgunther@gmail.com>
2020-11-19 22:50:27 -0700

    The frog can move!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    
+    
+    void Update()
+    {
+        horizontalMove= Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate() {
+        controller.Move(horizontalMove * runSpeed * Time.fixedDeltaTime, false, false);
+    }
+}
